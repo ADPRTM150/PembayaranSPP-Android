459eafa3563e53f853a4c4c4ef3781a286d48b37
ADPRTM150 <201951247@std.umk.ac.id>
2022-06-08 22:58:02 +0700

    Edit Adprtm

--- login_.php
+++ login_.php
@@ -1,26 +1,26 @@
 <?php
-if($_SERVER['REQUEST_METHOD']=='POST'){
-	//variabel untuk menyimpan kiriman dari form
-	$user = $_POST['username'];
-	$pass = $_POST['password'];
-	
-	if($user=='' || $pass==''){
-		echo "Form belum lengkap!!";
-	}else{
-		include "config/koneksi.php";
-		$sqlLogin = mysqli_query($konek, "SELECT * FROM admin 
+if (isset($_POST['simpan'])) {
+    //variabel untuk menyimpan kiriman dari form
+    $user = $_POST['username'];
+    $pass = $_POST['password'];
+
+    if ($user == '' || $pass == '') {
+        echo "Form belum lengkap!!";
+    } else {
+        include "config/koneksi.php";
+        $sqlLogin = mysqli_query($konek, "SELECT * FROM admin 
 						WHERE username='$user' AND password='$pass'");
-		$jml = mysqli_num_rows($sqlLogin);
-		$d=mysqli_fetch_array($sqlLogin);
-		if($jml > 0){
-			session_start();
-			$_SESSION['login']	= true;
-			$_SESSION['id']		= $d['idadmin'];
-			$_SESSION['username']=$d['username'];
-			
-			header('location:./index.php');
-		}else{
+        $jml = mysqli_num_rows($sqlLogin);
+        $d = mysqli_fetch_array($sqlLogin);
+        if ($jml > 0) {
+            session_start();
+            $_SESSION['login']    = true;
+            $_SESSION['id']        = $d['idadmin'];
+            $_SESSION['username'] = $d['username'];
+
+            header('location:./index.php');
+        } else {
             header("location:login.php");
-	}
-}
+        }
+    }
 }
